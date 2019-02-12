d14a68aad56bcbe17367367f1004157dcdabe84a
oerling@fb.com <oerling@fb.com>
2019-02-12 13:55:27 -0800

    test running script

---
+++ scripts/tests/run.sh
@@ -0,0 +1,11 @@
+
+../test.sh filters1_nulls.sql
+../test.sh filters1.sql
+../test.sh nulls1.sql
+../test.sh nulls2.sql
+../test.sh string1.sql
+../test.sh strings_filter1.sql
+../test.sh strings_struct1.sql
+../test.sh strings_struct_filter1.sql
+../test.sh strings_struct_nulls1.sql
+../test.sh strings_struct_nulls_filter1.sql
