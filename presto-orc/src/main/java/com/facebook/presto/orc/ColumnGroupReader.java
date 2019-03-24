a688737d8809cb906fcf66ede5361f5179869275
oerling@fb.com <oerling@fb.com>
2019-03-24 16:51:50 -0700

    Pushdown of constant columns
    Evaluates filters and filter functions dependent on constant columns at setup time.
    Supports filter functions on combinations of constant and non-constant columns.
    Adds constant columns in OrcRecordReader.resultPage().
    Fills missing fields of structs with nulls.
    Prunes the initial qualifying set in OrcRecordReader with any non-deterministic constant filters, evaluating these for every row.
    
    Adds a assertQuery signature for comparing Presto execution with
    different session settings.

--- presto-orc/src/main/java/com/facebook/presto/orc/ColumnGroupReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/ColumnGroupReader.java
@@ -19,6 +19,7 @@ import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.PageSourceOptions.FilterFunction;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.DictionaryBlock;
+import com.facebook.presto.spi.block.RunLengthEncodedBlock;
 import com.facebook.presto.spi.type.Type;
 
 import java.io.IOException;
@@ -66,6 +67,7 @@ public class ColumnGroupReader
     private int[] filterResults;
     private Map<Integer, StreamReader> channelToStreamReader;
     private int[] outputChannels;
+    private final Block[] constantBlocks;
 
     public ColumnGroupReader(
             StreamReader[] streamReaders,
@@ -77,11 +79,13 @@ public class ColumnGroupReader
             Map<Integer, Filter> filters,
             FilterFunction[] filterFunctions,
             boolean reorderFilters,
-            int ariaFlags)
+            int ariaFlags,
+            Block[] constantBlocks)
     {
         this.reorderFilters = reorderFilters;
         this.ariaFlags = ariaFlags;
         this.outputChannels = requireNonNull(outputChannels, "outputChannels is null");
+        this.constantBlocks = constantBlocks;
         channelToStreamReader = new HashMap();
         for (int i = 0; i < channelColumns.length; i++) {
             int columnIndex = channelColumns[i];
@@ -221,6 +225,9 @@ public class ColumnGroupReader
         int functionIdx = 0;
         int[] channels = function.getInputChannels();
         for (int channel : channels) {
+            if (constantBlocks[channel] != null) {
+                continue;
+            }
             int idx = findChannelIdx(channel);
             if (idx > firstNonFilter) {
                 // Move this to position firstNonFilter.
@@ -364,6 +371,10 @@ public class ColumnGroupReader
             if (channel == -1) {
                 continue;
             }
+            if (constantBlocks[channel] != null) {
+                blocks[channel] = new RunLengthEncodedBlock(constantBlocks[channel].getRegion(0, 1), numFirstRows);
+                continue;
+            }
             StreamReader reader = channelToStreamReader.get(i);
             if (reader != null) {
                 blocks[channel] = reader.getBlock(numFirstRows, reuseBlocks);
@@ -479,6 +490,9 @@ public class ColumnGroupReader
     private Block makeFilterFunctionInputBlock(int channelIdx, int streamIdx, int numRows, FilterFunction function)
     {
         int channel = function.getInputChannels()[channelIdx];
+        if (constantBlocks[channel] != null) {
+            return new RunLengthEncodedBlock(constantBlocks[channel].getRegion(0, 1), numRows);
+        }
         int[][] rowNumberMaps = function.getChannelRowNumberMaps();
         boolean mustCopyMap = false;
         int[] map = null;
