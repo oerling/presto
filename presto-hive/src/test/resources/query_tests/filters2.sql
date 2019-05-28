a009c0147a66779de86c464207aa175254480c10
oerling@fb.com <oerling@fb.com>
2019-05-27 19:00:42 -0700

    tests

--- presto-hive/src/test/resources/query_tests/filters2.sql
+++ presto-hive/src/test/resources/query_tests/filters2.sql
@@ -8,3 +8,25 @@ and comment > 'f'
 and comment > concat(shipmode, shipinstruct)
 ;
 
+
+select count(*) from lineitem_aria_nulls where float_quantity in (1, 2, 4)
+;
+select count(*) from lineitem_aria where quantity in (1, 2, 4)
+;
+
+select count(*) from lineitem_aria where float_quantity in (1, 2, 4)
+;
+
+select count(*) from lineitem_aria where long_decimal_discount in (0.01, 0.02)
+;
+
+select count(*) from lineitem_aria where short_decimal_discount in (0.01, 0.02)
+;
+
+select is_returned from lineitem_aria where is_returned in (true, false)
+;
+select partkey, suppkey, quantity, comment, extendedprice, returnflag, float_quantity, shipinstruct,
+  json_format(cast(order_part_supp_array as json)),  json_format(cast(order_part_supp_map as json)), json_format(cast(string_map as json))
+from lineitem_aria_nulls where linenumber = 1 and comment like '%f%';
+
+
