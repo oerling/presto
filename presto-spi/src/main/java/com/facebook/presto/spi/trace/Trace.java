aff06ff0925e1f2835700e583c817475b6fbb0ca
root <root@oerling-fedora-mj071frm.dhcp.thefacebook.com>
2019-09-18 14:05:59 -0700

    formatting

--- presto-spi/src/main/java/com/facebook/presto/spi/trace/Trace.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/trace/Trace.java
@@ -28,7 +28,7 @@ public class Trace
     private static BufferedWriter traceWriter;
     private static long openTimeMillis;
     private static boolean printTime;
-    
+
     private Trace() {}
 
     public static void readTraceSettings()
@@ -72,7 +72,7 @@ public class Trace
 
     public static void trace(String text)
     {
-        synchronized(Trace.class) {
+        synchronized (Trace.class) {
             try {
                 if (traceWriter == null) {
                     traceWriter = Files.newBufferedWriter(Paths.get("/tmp/presto.out"), CREATE);
